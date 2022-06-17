bab6f8fd35f9439edbdcec4e4bb7f31c9697cc10
Агабекян Эдгар Артемович <agabekyan.e@milandr.ru>
2022-06-17 09:24:27 +0500

    RefomatCode

--- src/main/java/TicketRepository.java
+++ src/main/java/TicketRepository.java
@@ -4,12 +4,14 @@ public class TicketRepository {
     public void save(Ticket ticket) {
         Ticket[] tmp = new Ticket[tickets.length + 1];
         for (int i = 0; i < tickets.length; i++) {
-            tmp[i] =tickets[i];
+            tmp[i] = tickets[i];
 
         }
         tmp[tmp.length - 1] = ticket;
         tickets = tmp;
     }
 
-    public Ticket[] findTicked() {return tickets;}
+    public Ticket[] findTicked() {
+        return tickets;
+    }
 }
--- src/test/java/TicketManagerTest.java
+++ src/test/java/TicketManagerTest.java
@@ -6,12 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TicketManagerTest {
 
-    Ticket ticket1 = new Ticket(1, 3500,"SVO", "SVP", 620);
-    Ticket ticket2 = new Ticket(2, 2800,"SVO", "SVS", 450);
-    Ticket ticket3 = new Ticket(3, 1370,"SVO", "SVP", 850);
-    Ticket ticket4 = new Ticket(4, 2900,"SVO", "SVP", 230);
-    Ticket ticket5 = new Ticket(5, 3100,"SVB", "SVP", 480);
-    Ticket ticket6 = new Ticket(6, 1370,"SVO", "SVP", 470);
+    Ticket ticket1 = new Ticket(1, 3500, "SVO", "SVP", 620);
+    Ticket ticket2 = new Ticket(2, 2800, "SVO", "SVS", 450);
+    Ticket ticket3 = new Ticket(3, 1370, "SVO", "SVP", 850);
+    Ticket ticket4 = new Ticket(4, 2900, "SVO", "SVP", 230);
+    Ticket ticket5 = new Ticket(5, 3100, "SVB", "SVP", 480);
+    Ticket ticket6 = new Ticket(6, 1370, "SVO", "SVP", 470);
 
 
     @Test
@@ -28,8 +28,9 @@ public class TicketManagerTest {
         Ticket[] actual = manager.findAll("SVO", "SVP");
         Ticket[] expected = {ticket3, ticket4, ticket1};
 
-        assertArrayEquals(expected,actual);
+        assertArrayEquals(expected, actual);
     }
+
     @Test
     public void shouldFindAndSortTicketNoMatch() {
         TicketRepository repo = new TicketRepository();
@@ -44,8 +45,9 @@ public class TicketManagerTest {
         Ticket[] actual = manager.findAll("SVO", "NVP");
         Ticket[] expected = {};
 
-        assertArrayEquals(expected,actual);
+        assertArrayEquals(expected, actual);
     }
+
     @Test
     public void shouldFindAndPriceMatch() {
         TicketRepository repo = new TicketRepository();
@@ -62,6 +64,6 @@ public class TicketManagerTest {
         Ticket[] actual = manager.findAll("SVO", "SVP");
         Ticket[] expected = {ticket3, ticket6, ticket4, ticket1};
 
-        assertArrayEquals(expected,actual);
+        assertArrayEquals(expected, actual);
     }
 }
